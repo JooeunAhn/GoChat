244102263c5f97ece906e8d44925a5554fd59d42
JooeunAhn <jflow@hanyang.ac.kr>
2016-10-13 16:25:52 +0900

    Initial Commit

---
+++ GoChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoChat
+//
+//  Created by JooEun,Ahn on 2016. 10. 13..
+//  Copyright © 2016년 AhnJoo-Eun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
